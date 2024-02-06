b6b7f4032233e374ca10c928bfb7b355450da616
RaRhAeu <radzim_ko@wp.pl>
2024-02-06 10:43:54 +0100

    f

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,3 +9,8 @@ def test_create_database():
 def test_database_from_env():
     db = Database.from_env()
     assert isinstance(db, Database)
+
+
+def test_database_supports(db):
+    assert db.supports_returning
+    assert db.supports_on_conflict
